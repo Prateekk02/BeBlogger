d1af3730d6fb98b822301abb129be5f886a5573c
Prateekk02 <prateek.kumar2002@gmail.com>
2024-11-14 14:37:13 +0530

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
-export default function App(){
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return <>
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux";
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'
 
-    
-    <h1>Be Blogger  </h1>
-  </>
+export default function App(){
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userData}));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .catch(() =>{
+      alert("User not fount");
+    })
+    .finally(() =>{
+      setLoading(false);
+    })
+  },[])
+
+  return !loading ? (
+    <>
+        <div className="min-h-screen flex flex-wrap bg-gray-400 content-between">
+            <div className="w-full">
+              <Header />
+                  <main>
+                      HELLO THERE!!!
+                  </main>
+              <Footer />
+            </div> 
+        </div>
+    </>
+  ) : (
+    <>
+       <h1>Skeleton Loading...</h1> 
+    </>
+  )
+
+  
 }
